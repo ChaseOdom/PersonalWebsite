ea43268127b889c774c24af3c7bfc613b7e4dd86
ChaseOdom <chase7771000@gmail.com>
2024-09-16 18:41:51 -0500

    initial commit

---
+++ app/test/page.tsx
@@ -0,0 +1,14 @@
+'use client'
+import React, {useRef} from "react";
+import {GameCard} from "@/components/gamecard2";
+import { Button, Card, CardBody, Tab, Tabs } from "@nextui-org/react";
+import stevon from "@/components/pictures/ARR.png";
+
+export default function App() {
+
+  return (
+    <div className="flex flex-row items-center justify-center gap-4">
+      Hello
+    </div>
+  );
+}
---
+++ components/gamecard2.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import {Card, CardHeader, CardBody} from "@nextui-org/react";
+import Image, { StaticImageData } from 'next/image';
+import styles from './App.module.css';
+
+interface cardProps {
+    title: string;
+    description: string;
+    selected: boolean;
+    cardImg: StaticImageData;
+    cardClicked: (message: number) => void;
+    index: number;
+}
+
+export function GameCard(props: cardProps) {
+
+    function HandlePressed(){
+        props.cardClicked(props.index);
+    }
+
+    return(
+        <Card className={props.selected ? styles.backgroundPressed : styles.backgroundUnpressed} isHoverable={true} isPressable={true} onPress={HandlePressed}>
+            <CardHeader className="pb-0 pt-2 px-4 flex-col items-center">
+                <p className={styles.test}>{props.title}</p>
+                <small className="text-default-500">{props.description}</small>
+            </CardHeader>
+            <CardBody className="overflow-visible py-2 place-items-baseline">
+                <Image className="object-cover" src={props.cardImg} width={150} height={150}alt="Stevon"/>
+            </CardBody>
+        </Card>
+);
+}
