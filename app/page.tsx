38d6574e2535a74adc9854892e208e5e8e37da3b
ChaseOdom <chase7771000@gmail.com>
2024-09-17 17:46:09 -0500

    added more stuff

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { button as buttonStyles } from "@nextui-org/theme";
 import { Card } from "@nextui-org/react";
 
 import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
+import { title, titleRight, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
 import { redirect } from "next/dist/server/api-utils";
 import { useRouter } from 'next/navigation';
@@ -15,27 +15,26 @@ export default function Home() {
   const router = useRouter();
 
   function pressedCard(cardPressed: string){
-    if(cardPressed == "resume"){
-      router.push('/resume');
-    }
+    router.push('/' + cardPressed);
   }
 
   return (
     <section className="grid grid-cols-4 gap-4">
-      <Card className="col-span-3 p-4" isPressable={true} onPress={()=>pressedCard("resume")}>
+      <Card className="col-span-2 p-8" isPressable={true} onPress={()=>pressedCard("resume")}>
         <h1 className={title({color: "cyan"})}>Resume</h1>
         <p>Very cool stuff happening on that pdf. Check it out and hire me!</p>
       </Card>
       <div></div>
       <div></div>
-      <Card className="col-span-3 p-4" isPressable={true} onPress={()=>pressedCard("resume")}>
-        <h1 className={title({color: "yellow"})}>Games</h1>
+      <div></div>
+      <Card className="col-span-2 p-8" isPressable={true} onPress={()=>pressedCard("games")}>
+        <h1 className={titleRight({color: "yellow"})}>Games</h1>
         <p>I make a bunch of games. Take a look!</p>
       </Card>
-      <Card className="col-span-3 p-4" isPressable={true} onPress={()=>pressedCard("resume")}>
+      <Card className="col-span-2 p-8" isPressable={true} onPress={()=>pressedCard("aboutme")}>
         <h1 className={title({color: "pink"})}>About Me</h1>
         <p>
-          Learn more about my hobbies and stuff. PD is the best!
+          Learn more about my hobbies and stuff
         </p>
       </Card>
     </section>
