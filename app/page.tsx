6b54e13c73508a702fe6da4d2d2bf464f3300c57
ChaseOdom <chase7771000@gmail.com>
2024-09-18 19:02:57 -0500

    fix homepage

--- app/page.tsx
+++ app/page.tsx
@@ -20,18 +20,17 @@ export default function Home() {
 
   return (
     <section className="grid grid-cols-4 gap-4">
-      <Card className="col-span-2 p-8" isPressable={true} onPress={()=>pressedCard("resume")}>
-        <h1 className={title({color: "cyan"})}>Resume</h1>
+      <Card className="col-span-3 p-8" isPressable={true} onPress={()=>pressedCard("resume")}>
+        <h1 className={title({color: "blue"})}>Resume</h1>
         <p>Very cool stuff happening on that pdf. Check it out and hire me!</p>
       </Card>
       <div></div>
       <div></div>
-      <div></div>
-      <Card className="col-span-2 p-8" isPressable={true} onPress={()=>pressedCard("games")}>
+      <Card className="col-span-3 p-8" isPressable={true} onPress={()=>pressedCard("games")}>
         <h1 className={titleRight({color: "yellow"})}>Games</h1>
         <p>I make a bunch of games. Take a look!</p>
       </Card>
-      <Card className="col-span-2 p-8" isPressable={true} onPress={()=>pressedCard("aboutme")}>
+      <Card className="col-span-3 p-8" isPressable={true} onPress={()=>pressedCard("aboutme")}>
         <h1 className={title({color: "pink"})}>About Me</h1>
         <p>
           Learn more about my hobbies and stuff
