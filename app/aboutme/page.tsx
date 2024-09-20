40ff9defaea0eb3dfbedf28cda65b4563df17509
ChaseOdom <chase7771000@gmail.com>
2024-09-20 01:02:38 -0500

    made mobile for about me better

--- app/aboutme/page.tsx
+++ app/aboutme/page.tsx
@@ -12,11 +12,11 @@ export default function AboutPage() {
       </div>
     
       <div className="m-5 flex items-center">
-        <div className="inline-block w-1/2 m-5">
+        <div className={styles.paragraphContainerLeft}>
           <p className={styles.paragraphFont}>I&apos;ve done every Escape Room at Escape Hour Austin.
             The christmas escape room had little cups so that you could self-serve hot cocoa. I&apos;d highly recommend going! </p>
         </div>
-        <div className="flex w-1/2">
+        <div className="flex w-3/4">
           <img src="/escape.JPG"></img>
         </div>
       </div>
@@ -26,7 +26,7 @@ export default function AboutPage() {
           <track default kind="captions" src="" />
           <source src="/piano2.mp4"></source>
         </video>
-        <div className="inline-block w-1/2 space-x-5">
+        <div className={styles.paragraphContainerRight}>
           <p className={styles.paragraphFont}> I used to play a lot of piano with sheet music, but I was never good at sight reading.
             In the last year or so I&apos;ve been getting into freestyling. Here&apos;s a video of me doing a little e minor jig.
             I especially like blues freestyles. </p>
@@ -34,21 +34,21 @@ export default function AboutPage() {
       </div>
 
       <div className="m-5 flex items-center">
-        <div className="inline-block w-1/2 m-5">
+        <div className={styles.paragraphContainerLeft}>
           <p className={styles.paragraphFont}> I love hamsters! They&apos;re the best pet. In college I wanted a low maintenance pet, but found out I was allergic to cats! D:
             Hamsters are great workarounds! haha they&apos;re cuter than cats, but you can&apos;t play with them as much. So that&apos;s the compromise you run getting one.
           </p>
         </div>
-        <div className="flex w-1/3">
+        <div className="flex w-1/2">
           <img src="/hamster.jpg"></img>
         </div>
       </div>
 
       <div className="m-5 flex items-center">
-        <div className="flex w-1/3">
+        <div className="flex w-1/2">
           <img src="/Car.jpg"></img>
         </div>
-        <div className="inline-block w-1/2 m-5">
+        <div className={styles.paragraphContainerRight}>
           <p className={styles.paragraphFont}> I&apos;ve had 2 mustangs in my life My dad got my first when I was in highschool and it got me through college. It was a maroon 2006 coupe.
             I&apos;ve been using my 2017 gt convertible for about 4 years now and I&apos;m going to run it into the ground and never sell it.
             There&apos;s no other muscle car for me than a mustang!
