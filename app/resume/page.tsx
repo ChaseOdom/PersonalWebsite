9680ed9acc72b8cde2c2b4da880df394e021f705
ChaseOdom <chase7771000@gmail.com>
2024-09-20 00:44:17 -0500

    improved pdf view in mobile

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -1,10 +1,11 @@
 import React, {useRef} from "react";
+import styles from './resumecss.module.css';
 
 export default function App() {
 
     return (
-        <div className="flex flex-col items-center gap-4">
-            <iframe src="https://drive.google.com/file/d/13AOwOcLn8XPb04xonhDM8cpmWapor-TM/preview" title='resume' width="640" height="900" allow="autoplay"></iframe>
+        <div className="flex w-full justify-center items-center">
+            <iframe src="https://drive.google.com/file/d/13AOwOcLn8XPb04xonhDM8cpmWapor-TM/preview" title='resume' className={styles.pdfStyle} allow="autoplay"></iframe>
         </div>
     );
 }
